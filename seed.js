c0e4a7227062388dde7f265f03500442be5a2f85
Danel <danel.li@mail.ru>
2026-02-06 13:33:53 +0500

    Update project files

--- seed.js
+++ seed.js
@@ -4,14 +4,70 @@ const Product = require('./models/Product');
 const User = require('./models/User');
 
 const jewelryItems = [
-    { name: "Royal Diamond Ring", price: 2500, category: "Rings", specs: { metal: "Platinum", stone: "Diamond" }, stock: 5, reviews: [{user: "Alice", comment: "Stunning!"}] },
-    { name: "Emerald Serenity Necklace", price: 1800, category: "Necklaces", specs: { metal: "Yellow Gold", stone: "Emerald" }, stock: 3 },
-    { name: "Midnight Sapphire Studs", price: 950, category: "Earrings", specs: { metal: "White Gold", stone: "Sapphire" }, stock: 8 },
-    { name: "Golden Leaf Bracelet", price: 400, category: "Bracelets", specs: { metal: "24K Gold", stone: "None" }, stock: 12 },
-    { name: "Pearl Elegance Set", price: 1200, category: "Sets", specs: { metal: "Silver", stone: "Pearl" }, stock: 4 },
-    { name: "Ruby Fire Pendant", price: 1500, category: "Necklaces", specs: { metal: "Rose Gold", stone: "Ruby" }, stock: 6 },
-    { name: "Silver Moon Hoops", price: 150, category: "Earrings", specs: { metal: "Silver", stone: "None" }, stock: 20 },
-    { name: "Amethyst Charm", price: 300, category: "Bracelets", specs: { metal: "Gold Plated", stone: "Amethyst" }, stock: 10 }
+    {
+        name: "Royal Diamond Ring",
+        price: 2500,
+        category: "Rings",
+        specs: { metal: "Platinum", stone: "Diamond" },
+        stock: 5,
+        image: "https://images.unsplash.com/photo-1605100804763-247f67b3557e?w=800"
+    },
+    {
+        name: "Emerald Serenity Necklace",
+        price: 1800,
+        category: "Necklaces",
+        specs: { metal: "Yellow Gold", stone: "Emerald" },
+        stock: 3,
+        image: "https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?w=800"
+    },
+    {
+        name: "Midnight Sapphire Studs",
+        price: 950,
+        category: "Earrings",
+        specs: { metal: "White Gold", stone: "Sapphire" },
+        stock: 8,
+        image: "https://images.unsplash.com/photo-1630019852942-f89202989a59?w=800"
+    },
+    {
+        name: "Golden Leaf Bracelet",
+        price: 400,
+        category: "Bracelets",
+        specs: { metal: "24K Gold", stone: "None" },
+        stock: 12,
+        image: "https://images.unsplash.com/photo-1611591437281-460bfbe1220a?w=800"
+    },
+    {
+        name: "Pearl Elegance Set",
+        price: 1200,
+        category: "Sets",
+        specs: { metal: "Silver", stone: "Pearl" },
+        stock: 4,
+        image: "https://images.unsplash.com/photo-1535633302704-b02f4faad311?w=800"
+    },
+    {
+        name: "Ruby Fire Pendant",
+        price: 1500,
+        category: "Necklaces",
+        specs: { metal: "Rose Gold", stone: "Ruby" },
+        stock: 6,
+        image: "https://images.unsplash.com/photo-1602173574767-37ac01994b2a?w=800"
+    },
+    {
+        name: "Silver Moon Hoops",
+        price: 150,
+        category: "Earrings",
+        specs: { metal: "Silver", stone: "None" },
+        stock: 20,
+        image: "https://images.unsplash.com/photo-1535633602411-da6379b07ff0?w=800"
+    },
+    {
+        name: "Amethyst Charm",
+        price: 300,
+        category: "Bracelets",
+        specs: { metal: "Gold Plated", stone: "Amethyst" },
+        stock: 10,
+        image: "https://images.unsplash.com/photo-1617038220319-276d3cfab638?w=800"
+    }
 ];
 
 mongoose.connect(process.env.MONGO_URI)
--- updatePhotos.js
+++ updatePhotos.js
@@ -2,17 +2,6 @@ const mongoose = require('mongoose');
 const Product = require('./models/Product');
 mongoose.connect('mongodb+srv://shukurbayassylay_db_user:Assylay2007.@cluster0.7dqmxpg.mongodb.net/');
 
-const photos = {
-    "Royal Diamond Ring": "https://images.unsplash.com/photo-1605100804763-247f67b3557e?w=800",
-    "Emerald Serenity Necklace": "https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?w=800",
-    "Midnight Sapphire Studs": "https://images.unsplash.com/photo-1635767791008-3cbaaa5df335?w=800",
-    "Golden Leaf Bracelet": "https://images.unsplash.com/photo-1611591437281-460bfbe1220a?w=800",
-    "Pearl Elegance Set": "https://images.unsplash.com/photo-1506630448388-4e683c67ddb0?w=800",
-    "Ruby Fire Pendant": "https://images.unsplash.com/photo-1602173574767-37ac01994b2a?w=800",
-    "Silver Moon Hoops": "https://images.unsplash.com/photo-1630019058353-524407960100?w=800",
-    "Amethyst Charm": "https://images.unsplash.com/photo-1617038260897-41a1f14a8ca0?w=800"
-
-};
 
 async function updateProducts() {
     for (const [name, url] of Object.entries(photos)) {
